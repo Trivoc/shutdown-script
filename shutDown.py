bf740ec16e42762ce53a2fa8d83fd4949a87b09b
Victor 'Trivoc' Nilsson <trivoc92@gmail.com>
2017-04-08 16:04:40 +0200

    Structure for waiting until shutdown in place

--- shutDown.py
+++ shutDown.py
@@ -4,17 +4,17 @@ import time
 start = time.time()
 print("Hello there!")
 
+limitStr = input("How long until shutdown? (XXhYYmZZs) \n")
+limit = int(limitStr)
+if limit <= 10 : 
+    time.sleep(limit)
+else: 
+    time.sleep(limit-10)
+    print("10 seconds left")
+    time.sleep(10)
+print("Done!")
 
-
-clock = time.time()
-while (time.time() - start) < 5:
-	inp = input("Want to quit?\n")
-	if inp == "yes":
-		break
-	
-print("Time out")
 end = time.time()
-print("Elapsed time: " + str((end - start)*1000000 ) + " ns")	
-
+print("Elapsed limit: " + str(end - start))
 
 #subprocess.call(["shutdown", "/s"])	
