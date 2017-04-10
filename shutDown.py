230b5de9fb5b224fc41a8670a4113c0f460ef4d2
Victor 'Trivoc' Nilsson <trivoc92@gmail.com>
2017-04-10 16:50:49 +0200

    Only syscalls left

--- shutDown.py
+++ shutDown.py
@@ -1,20 +1,46 @@
-import subprocess
+import platform
 import time
+import timeInput
+from sys import stdout
+from subprocess import call
 
-start = time.time()
-print("Hello there!")
+def sleepLoop(remainder) : 
+    while remainder  > 0 :
+        print("    Remaining: " + timeInput.formattedTimeString(remainder) + "    ", end='\r')
+        time.sleep(1)
+        remainder -= 1
+    stdout.flush()
+    print("\nTime up!")
 
-limitStr = input("How long until shutdown? (XXhYYmZZs) \n")
-limit = int(limitStr)
-if limit <= 10 : 
-    time.sleep(limit)
-else: 
-    time.sleep(limit-10)
-    print("10 seconds left")
-    time.sleep(10)
-print("Done!")
 
-end = time.time()
-print("Elapsed limit: " + str(end - start))
+def countDown(seconds) : 
+    part = seconds % 10
+    time.sleep(part)
+    seconds = seconds - part
+    seconds = sleepLoop(3600, seconds)
+    seconds = sleepLoop(300, seconds)
+    seconds = sleepLoop(10, seconds)
+    print("\nTime up!")
+
+if (platform.system() == "Linux") : 
+    print("Identified Linux platform")
+
+limitStr = input("How long until shutdown? (X Y Z (hours minutes seconds), or q to quit) \n")
+
+if(limitStr == ("q" or "Q" or "")) : 
+    print("No time given, quitting!")
+    quit()
+else : 
+    specTime = timeInput.getTime(limitStr)
+    print("Setting time to " + timeInput.formattedTimeString(specTime) + "\n")
+    call(["stty", "-echo"])
+    sleepLoop(specTime)
+    call(["stty", "echo"])
+
+
+
 
 #subprocess.call(["shutdown", "/s"])	
+
+
+
---
+++ timeInput.py
@@ -0,0 +1,55 @@
+import re
+
+def getTime(n) : 
+    specTime = re.sub(r'\W+', ' ', n)
+    specTime = specTime.split(" ")
+    if len(specTime) > 4 or len(specTime) < 1 : 
+        print("Faulty input format, try again")
+    else : 
+        result = 0
+        for token in specTime : 
+            formatted = getSpecific(re.sub('[^0-9dhmsDHMS]+', '', token))
+            try : 
+                result += formatted
+            except TypeError : 
+                print("Format error on token " + token)
+        return result
+
+def getSpecific(timeStamp) : 
+    if "d" in timeStamp or "D" in timeStamp : 
+        timeStamp = timeStamp.replace("d" or "D", "")
+        return convertToTime(timeStamp, (3600*24))
+    if "h" in timeStamp or "H" in timeStamp : 
+        timeStamp = timeStamp.replace("h" or "H", "")
+        return convertToTime(timeStamp, 3600)
+    if "m" in timeStamp or "M" in timeStamp : 
+        timeStamp = timeStamp.replace("m" or "M", "")
+        return convertToTime(timeStamp, 60)
+    if "s" in timeStamp or "S" in timeStamp : 
+        timeStamp = timeStamp.replace("s" or "S", "")
+
+    return convertToTime(timeStamp, 1)
+
+
+    
+
+def convertToTime(time, multiplier) : 
+    try : 
+        seconds = int(time)
+        return seconds*multiplier
+    except ValueError : 
+        return 0
+
+#Takes time in seconds, returns formatted string
+def formattedTimeString(time) : 
+    days = time // (3600 * 24)
+    remains = time - days * (3600 * 24)
+    hours = remains // 3600
+    remains = remains - hours * 3600
+    minutes = remains // 60 
+    remains = remains - minutes * 60
+    seconds = remains
+    return (str(days) + " days, " + 
+            str(hours) + " hours, " + 
+            str(minutes) + " minutes, " + 
+            str(seconds) + " seconds")
